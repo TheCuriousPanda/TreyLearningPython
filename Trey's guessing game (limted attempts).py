11355add3641c53125f56b080e876f4732ac7ccf
Spencer P <stpoz@live.com>
2014-05-17 15:12:00 -0600

    Fixed bugs
    Fixed a forgotten end-parentheses and indented line problems

--- Trey's guessing game (limted attempts).py
+++ Trey's guessing game (limted attempts).py
@@ -3,11 +3,10 @@
 attempts=3
 solution=7
 for numberofattempts in range(1,attempts+1):
-    userguess=int(input("Guess a number between 1 and 10")
-                  if userguess==solution:
-                      print("You are correct!")
-                      break
-                  else
-                      print("Sorry that is not correct")
-                      print("You have" attempts-numbersofattempts,"tries left")
-                  
+	userguess=int(input("Guess a number between 1 and 10: "))
+	if userguess==solution:
+		print("You are correct!")
+		break
+	else:
+		print("Sorry that is not correct")
+		print("You have {} tries left".format(attempts-numberofattempts))
